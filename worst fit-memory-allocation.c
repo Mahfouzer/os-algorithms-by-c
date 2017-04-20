f8ed0ac35d866977d901dcb6c96ca2b61289e0d4
AmrAnwar <amranwar945@gmail.com>
2017-04-21 00:24:12 +0200

    add first simple report

--- worst fit-memory-allocation.c
+++ worst fit-memory-allocation.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 int main()
 {
-    printf("First Fit \n");
+    printf("Worst Fit \n");
    int max , i , j ,tmp;
-   printf("enter max \n");
+   printf("\nenter max blocks: ");
    scanf("%d",&max);
    int b_n, f_n;
-   printf("enter file number and block number \n ");
+   printf("\nenter file number and block number: ");
    scanf("%d %d",&f_n,&b_n);
    int f[max],b[max],b_v[max],ff[max],frag[max];
     for ( i =0 ; i < max; i++)b_v[i]=0;
     for ( i =0 ; i < max; i++)frag[i]=0;
-
+ 
    for (i =0 ; i < b_n ; i++){
-        printf("enter block %d size: ",i);
+        printf("\nenter block %d size: ",i+1);
         scanf("%d",&b[i]);
    }
    for (i =0 ; i < f_n ; i++){
-        printf("enter file %d size",i);
+        printf("\nenter file %d size: ",i+1);
         scanf("%d",&f[i]);
    }
     for ( i =0 ; i < f_n; i++){
@@ -34,28 +34,20 @@ int main()
             }
         }
         if( flag== 1){
-        //
-        if(maxx ==0){
-
                b_v[max_index]=1;
                 ff[i]=max_index;
                 if (frag[ i] == 0) frag[i]=maxx;
                 else frag[i]-=maxx;
+ 
         }
         else{
-                ff[i]=max_index;
-                if (frag[ i] == 0) frag[i]=maxx;
-                else frag[i]-=maxx;
-        }
-        }
-        else{
-            printf("no space \n ");
+            printf("\n no space");
             return 0;
         }
-
+ 
     }
     for( i =0 ; i  < f_n; i++){
-        printf(" file %d  , size %d , in block %d , fragmentation equal  %d \n", i,f[i] , ff[i], frag[i]  );
+        printf(" \n file %d  , size %d , in block %d , fragmentation equal  %d ", i+1,f[i] , ff[i], frag[i]  );
     }
     return 0;
 }
