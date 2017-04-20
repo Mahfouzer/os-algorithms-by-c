f32ae917cedcc3d1a9d2fdfc2afdcf4a12badebc
AmrAnwar <amranwar945@gmail.com>
2017-04-21 00:29:38 +0200

    add simple second report

---
+++ float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+int main()
+{
+    printf(" max float %E \n",FLT_MAX);
+    printf(" dig float %E \n",FLT_DIG);
+    printf(" min float %E \n",FLT_MIN);
+    printf(" Epsilion float %E \n",FLT_EPSILON);
+
+    return 0;
+}
+
