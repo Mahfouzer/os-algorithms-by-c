67595d0257a65b3a79307c224861aab9cac5df4c
Amr Anwar <AmrAnwar@users.noreply.github.com>
2017-04-27 04:41:29 +0200

    fix error

--- swap_function.c
+++ swap_function.c
@@ -2,21 +2,21 @@
 void swap(int *x, int *y) {
 
    int temp;
-   temp = *x;    
-   *x = *y;     
-   *y = temp;    
-  
+   temp = *x;
+   *x = *y;
+   *y = temp;
+
    return;
-} 
+}
 int main () {
 
 
-   int a,b;cin>>a>>b;
+   int a,b;scanf("%d %d",&a,&b);
    printf(" value of a : %d\n", a );
    printf(" value of b : %d\n", b );
    swap(&a, &b);
    printf("after swap, value of a : %d\n", a );
    printf("after swap, value of b : %d\n", b );
- 
+
    return 0;
 }
