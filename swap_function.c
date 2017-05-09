f9d49e97b050f5af8e674ecba5b21da821cc2536
Nader Mahfouz <mahfouzs96@gmail.com>
2017-05-09 18:05:06 +0200

    Update swap_function.c
    removed white spaces

--- swap_function.c
+++ swap_function.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 void swap(int *x, int *y) {
-
    int temp;
    temp = *x;
    *x = *y;
    *y = temp;
-
    return;
 }
 int main () {
-
-
    int a,b;scanf("%d %d",&a,&b);
    printf(" value of a : %d\n", a );
    printf(" value of b : %d\n", b );
    swap(&a, &b);
    printf("after swap, value of a : %d\n", a );
    printf("after swap, value of b : %d\n", b );
-
    return 0;
 }
